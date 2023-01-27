24eb6c6ed40be8a62aaaf2c09bffcecef9c1d81d
m0wmt <you@example.com>
2023-01-27 16:08:11 +0000

    Update config example

--- include/config_example.h
+++ include/config_example.h
@@ -4,5 +4,6 @@
 #define WIFI_PASSWORD "PASSWORD"
 #define SNTP_TIME_SERVER "pool.ntp.org"
 #define WEATHER_URL "https://api.openweathermap.org/data/2.5/weather?units=metric&mode=json&lat=51.50&lon=0.00&appid=api-key-here"
-#define FORECAST_URL "https://api.openweathermap.org/data/2.5/forecast?units=metric&mode=json&cnt=5&lat=51.50&lon=0.00&appid=api-key-here"
+#define FORECAST_URL "https://api.openweathermap.org/data/2.5/forecast?units=metric&mode=json&cnt=10&lat=51.50&lon=0.00&appid=api-key-here"
+#define DAILY_FORECAST_URL "https://api.openweathermap.org/data/2.5/forecast/daily?units=metric&mode=json&cnt=7&lat=51.50&lon=0.00&appid=api-key-here"
 #define LOCATION "London"
