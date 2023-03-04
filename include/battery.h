bf7d99af0c24eccb7e2ef90a86a2b96c513e8f51
m0wmt <you@example.com>
2023-03-04 15:27:36 +0000

    Increased low battery voltage level

--- include/battery.h
+++ include/battery.h
@@ -2,7 +2,7 @@
 
 #include <Arduino.h>
 
-const float LOW_BATTERY_VOLTAGE = 3.30; // warn user battery low!
+const float LOW_BATTERY_VOLTAGE = 3.40; // warn user battery low!
 
 uint32_t getBatteryVoltage(void);
 int calculateBatteryPercentage(double v);
